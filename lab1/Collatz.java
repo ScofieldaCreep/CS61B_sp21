ef7ebb86f51aef33c06d2efbd264d4f383418121
Scofield_TJU <tju_scofield@outlook.com>
2022-07-29 01:57:59 +0800

    down with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,7 +5,7 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        if (n == 128) {
             return 1;
         } else if (n == 5) {
             return 3 * n + 1;
@@ -15,6 +15,7 @@ public class Collatz {
     }
 
     public static void main(String[] args) {
+
         int n = 5;
         System.out.print(n + " ");
         while (n != 1) {
